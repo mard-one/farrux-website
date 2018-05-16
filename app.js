cd04d61dbe3e9fd16bcbdf622626bcc2417a4c65
Mardon Mashrabov <mardon.mashrabov@gmail.com>
2018-05-16 14:07:43 +0500

    home and about page

--- app.js
+++ app.js
@@ -4,11 +4,21 @@ var path = require('path');
 var app = express();
 
 app.set('view engine', 'ejs');
-app.use('public', express.static(path.join(__dirname + 'public')));
+app.use(express.static(__dirname + '/assets'));
 
+
+app.get('/home', function(req, res) {
+  res.redirect('/');
+})
+app.get('/about', function(req, res) {
+  res.render('about');
+})
+app.get('/activities', function(req, res) {
+  res.render('activities');
+})
 app.get('/', function(req, res) {
   res.render('home');
-})
+});
 
 app.set('port', 3000);
 app.listen(app.get('port'), function() {
